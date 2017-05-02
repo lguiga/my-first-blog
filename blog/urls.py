783a89c6552f8c2929df444714b9ece4a4650a14
schiefler <lguiga@gmail.com>
2017-05-01 23:34:08 -0300

    Funcionando a janela de exibição do post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='detail'),
 ]
