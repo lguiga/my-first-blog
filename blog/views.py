2abee3d3c00f02fe4d43d8714c4ace23631a24ed
schiefler <lguiga@gmail.com>
2017-05-01 21:14:59 -0300

    Changed the HTML fot the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	# Cria um QuerySet com uma lista dos posts ordenados pela data
+	return render(request, 'blog/post_list.html', {})
